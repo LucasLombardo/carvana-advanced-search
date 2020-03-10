5e18ee530bd07e2d504b0d2637dbb8e79d74cfc8
Lucas <lucasl@bu.edu>
2020-03-09 21:47:55 -0400

    adds docker compose for mysql and server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,12 @@
-import Koa from 'koa'
-import ApolloServer from './apollo'
+import Koa from "koa"
+import ApolloServer from "./apollo"
 
 const app = new Koa()
 
 app.use(ApolloServer.getMiddleware())
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: 8100 }, () =>
     console.log(
-        `🚀 Server ready at http://localhost:4000${ApolloServer.graphqlPath}`
+        `🚀 Server ready at http://localhost:8100${ApolloServer.graphqlPath}`
     )
 )
