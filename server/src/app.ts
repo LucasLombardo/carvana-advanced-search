c3402f139461deb44ddf11ac81a7ec2b877f03c7
Lucas <lucasl@bu.edu>
2020-03-14 15:49:53 -0400

    sets ctx.db to be the connection manager

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,20 @@ import Koa from "koa"
 import ApolloServer from "./apollo"
 import { createConnection } from "typeorm"
 
+console.log(process.env.DB_URI)
+
 const init = async () => {
-    await createConnection({
-        type: "mysql",
+    const connection = await createConnection({
+        type: `mysql`,
+        database: `db`,
         url: process.env.DB_URI,
+        entities: [`src/entities/**/model.ts`],
+        synchronize: true,
     })
 
     const app = new Koa()
+
+    app.context.db = connection.manager
 
     app.use(ApolloServer.getMiddleware())
 
