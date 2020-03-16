2256ca68aaf777b6d00965ef97b242486045a631
Lucas <lucasl@bu.edu>
2020-03-15 22:01:41 -0400

    adds resolver for single vehicle

--- server/src/entities/vehicles/resolvers.ts
+++ server/src/entities/vehicles/resolvers.ts
@@ -1,10 +1,22 @@
 import refreshInventory from "./helpers/refreshInventory"
+import { Vehicle } from "./model"
+import { IContext } from "../../app" // eslint-disable-line no-unused-vars
 
 export const resolvers = {
+    Query: {
+        vehicle: async (_parent: any, args: IVehicleArgs, ctx: IContext) => {
+            const res = await ctx.db.findOne(Vehicle, args.id)
+            return res || null
+        },
+    },
     Mutation: {
         refreshInventory: (_parent: any, _args: any, ctx: any) => {
             refreshInventory(ctx.db)
             return `OK`
         },
     },
+}
+
+interface IVehicleArgs {
+    id: number
 }
--- server/src/entities/vehicles/types.ts
+++ server/src/entities/vehicles/types.ts
@@ -1,7 +1,50 @@
 import { gql } from "apollo-server-koa"
 
 export const typeDefs = gql`
+    extend type Query {
+        vehicle(id: Int!): Vehicle
+    }
+
     extend type Mutation {
         refreshInventory: String!
+    }
+
+    type Vehicle {
+        id: Int!
+        make: String!
+        model: String!
+        price: Int!
+        totalPrice: Int!
+        lastFetched: String!
+        year: Int!
+        mileage: Int!
+        imageUrl: String!
+        transportCost: Int
+        trim: String
+        isOnDemand: Boolean
+        isPurchasePending: Boolean
+        daysUntilAvailable: Int
+        vehicleInventoryType: Int
+        vehicleLockType: Int
+        addedToCoreInventoryDateTime: String
+        vdpSlug: String
+        priceUpdateDate: String
+        previousPrice: Int
+        totalWriteDown: Int
+        locationId: Int
+        stockRecallStatusType: Int
+        vin: String
+        exteriorColor: String
+        interiorColor: String
+        driveType: String
+        mpgCity: Int
+        mpgHighway: Int
+        fuelDescription: String
+        transmission: String
+        numberOfKeys: Int
+        doors: Int
+        seating: String
+        bodyType: String
+        length: Int
     }
 `
