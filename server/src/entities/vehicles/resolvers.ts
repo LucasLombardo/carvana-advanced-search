cb2f81388a055d6b6fac2371183cdfd9850a2738
Lucas Lombardo <lucasl@bu.edu>
2020-04-18 18:42:07 -0400

    Feature/add vehicle filters (#8)
    * adds between filters for number values
    
    * fixes incorrect db port
    
    * adds filters for include, exclude, and bool types
    
    * adds sortBy and order options

--- server/src/entities/vehicles/resolvers.ts
+++ server/src/entities/vehicles/resolvers.ts
@@ -1,4 +1,5 @@
 import refreshInventory from "./helpers/refreshInventory"
+import { Between, Not, In, Like } from "typeorm"
 import { Vehicle } from "./model"
 import { IContext } from "../../app" // eslint-disable-line no-unused-vars
 
@@ -8,10 +9,49 @@ export const resolvers = {
             return await ctx.db.findOne(Vehicle, args.id)
         },
         vehicles: async (_parent: any, args: IVehiclesArgs, ctx: IContext) => {
-            const options = {
+            const options: { [key: string]: any } = {
                 skip: Math.max(args.skip, 0),
                 take: Math.max(args.limit, 1),
+                where: {
+                    // range filters
+                    price: Between(args.minPrice, args.maxPrice),
+                    mileage: Between(args.minMileage, args.maxMileage),
+                    year: Between(args.minYear, args.maxYear),
+                    length: Between(args.minLength, args.maxLength),
+                    numberOfKeys: Between(args.minKeys, args.maxKeys),
+                    doors: Between(args.minDoors, args.maxDoors),
+                },
             }
+
+            // include / exclude filters
+            if (args.makes || args.notMakes) {
+                options.where.make = args.makes
+                    ? In(args.makes)
+                    : Not(In(args.notMakes))
+            }
+
+            if (args.models || args.notModels) {
+                options.where.model = args.models
+                    ? In(args.models)
+                    : Not(In(args.notModels))
+            }
+
+            // include only filters
+            if (args.driveTypes) options.where.driveType = In(args.driveTypes)
+            if (args.bodyTypes) options.where.bodyType = In(args.bodyTypes)
+
+            if (args.tagsContain) {
+                options.where.vehicleTags = Like(`%${args.tagsContain}%`)
+            }
+
+            if (args.isPurchasePending !== undefined) {
+                options.where.isPurchasePending = args.isPurchasePending || null
+            }
+
+            if (args.sortBy) {
+                options.order = { [args.sortBy]: args.order }
+            }
+
             return await ctx.db.find(Vehicle, options)
         },
     },
@@ -30,4 +70,26 @@ interface IVehicleArgs {
 interface IVehiclesArgs {
     skip: number
     limit: number
+    minPrice: number
+    maxPrice: number
+    minMileage: number
+    maxMileage: number
+    minYear: number
+    maxYear: number
+    minLength: number
+    maxLength: number
+    minKeys: number
+    maxKeys: number
+    minDoors: number
+    maxDoors: number
+    makes: string[]
+    notMakes: string[]
+    models: string[]
+    notModels: string[]
+    driveTypes: string[]
+    bodyTypes: string[]
+    tagsContain: string
+    isPurchasePending: boolean
+    sortBy: "date" | "price" | "mileage" | "year"
+    order: "ASC" | "DESC"
 }
--- server/src/entities/vehicles/types.ts
+++ server/src/entities/vehicles/types.ts
@@ -3,11 +3,48 @@ import { gql } from "apollo-server-koa"
 export const typeDefs = gql`
     extend type Query {
         vehicle(id: Int!): Vehicle
-        vehicles(skip: Int = 0, limit: Int = 10): [Vehicle]!
+        vehicles(
+            skip: Int = 0
+            limit: Int = 10
+            minPrice: Int = 0
+            maxPrice: Int = 10000000
+            minMileage: Int = 0
+            maxMileage: Int = 10000000
+            minYear: Int = 0
+            maxYear: Int = 3000
+            minLength: Int = 0
+            maxLength: Int = 1000
+            minKeys: Int = 0
+            maxKeys: Int = 1000
+            minDoors: Int = 0
+            maxDoors: Int = 1000
+            makes: [String!]
+            notMakes: [String!]
+            models: [String!]
+            notModels: [String!]
+            driveTypes: [String!]
+            tagsContain: String
+            bodyTypes: [String!]
+            isPurchasePending: Boolean
+            sortBy: SortBy
+            order: Order = ASC
+        ): [Vehicle]!
     }
 
     extend type Mutation {
         refreshInventory: String!
+    }
+
+    enum SortBy {
+        addedToCoreInventoryDateTime
+        price
+        mileage
+        year
+    }
+
+    enum Order {
+        ASC
+        DESC
     }
 
     type Vehicle {
@@ -47,5 +84,6 @@ export const typeDefs = gql`
         seating: String
         bodyType: String
         length: Int
+        vehicleTags: String
     }
 `
