b765275bab972a05bc744c3e158f931f1ea64050
Lucas <lucasl@bu.edu>
2020-04-18 22:53:07 -0400

    adds apollo client

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,22 @@
-import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import fetch from "isomorphic-unfetch";
+import withApollo from "next-with-apollo";
 
-export default ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+const App = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <Component {...pageProps} />
+  </ApolloProvider>
 );
+
+export default withApollo(({ initialState }) => {
+  return new ApolloClient({
+    link: createHttpLink({
+      uri: process.env.GQL_ENDPOINT,
+      fetch: fetch,
+    }),
+    cache: new InMemoryCache().restore(initialState || {}),
+  });
+})(App);
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,30 @@
-export default () => (
-  <div>
-    <p>Hello World</p>
-  </div>
-);
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const QUERY = gql`
+  query {
+    vehicles {
+      make
+      model
+      price
+    }
+  }
+`;
+
+export default () => {
+  const { loading, data } = useQuery(QUERY);
+  if (loading || !data) {
+    return <h1>loading...</h1>;
+  }
+
+  return (
+    <div>
+      <p>Hello Apollo</p>
+      <ul>
+        {data.vehicles.map(({ make, model, price }) => (
+          <li>{make + " " + model + " $" + price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
